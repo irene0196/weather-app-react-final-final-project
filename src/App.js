04fa98e92ea3cecd6339a6280d0d14a34551c6c3
irene0196 <142917090+irene0196@users.noreply.github.com>
2024-03-04 20:45:31 +0000

    remove target blank

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
       <footer>
         <p>
           This project was coded by Irene and is{" "}
-          <a
-            href="https://github.com/irene0196/weather-app-react-final-final-project"
-            target="_blank"
-          >
+          <a href="https://github.com/irene0196/weather-app-react-final-final-project">
             open-sourced
           </a>
         </p>
