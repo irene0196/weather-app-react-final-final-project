d1aa0ca0d255c97295e8f7ef90f482f3965b926c
irene0196 <142917090+irene0196@users.noreply.github.com>
2024-03-08 15:11:07 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer>
-        <p>
-          This project was coded by Irene and is{" "}
-          <a href="https://github.com/irene0196/weather-app-react-final-final-project">
-            open-sourced
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <footer>
+          <p>
+            This project was coded by Irene and is{" "}
+            <a href="https://github.com/irene0196/weather-app-react-final-final-project">
+              open-sourced
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
